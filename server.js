e1b8a82d56d6f1e57870986aa324a4698f3f31d0
Ben West <bewest@gmail.com>
2015-07-01 02:25:38 -0700

    tweak?

--- server.js
+++ server.js
@@ -74,7 +74,7 @@ function createServer (opts) {
     res.header('X-Backend', v.url);
     var internal = '/x-accel-redirect/' + v.port + '/api/v1/status.json';
     console.log('internal!', internal);
-    res.header('X-Accel-Redirect', internal);
+    res.header('x-accel-redirect', internal);
     res.end( );
     next( );
   });
