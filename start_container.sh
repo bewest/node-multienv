895fd9f7683ff276fbeaa2cbcd8ede18f4823c5e
Ben West <bewest@gmail.com>
2020-09-12 09:47:15 -0700

    tweak entrypoint to allow running prepared tools
    Treat container more like a multipurpose tool, allowing it to run in docker or
    kubernetes using dedicated roles.

--- start_container.sh
+++ start_container.sh
@@ -4,6 +4,7 @@ echo starting container...
 whoami
 pwd
 env
+function setup_main ( ) {
 ls -alh /etc/nginx
 export INTERNAL_PORT=3434
 export REDIRECTOR_PORT=3636
@@ -30,18 +31,15 @@ rm -rf node_modules
 )
 npm install
 npm cache verify
-
-trap "finalize" TERM
-
-finalize ( ) {
-echo finalize
-kill -TERM $redirector_pid
-kill -TERM $multienv_pid
 }
 
+
+PM2=./node_modules/.bin/pm2
+RUNTIME=$PM2-runtime
+
+function main ( ) {
 export CLUSTER_CONSUL_ID="cluster:$HOSTNAME"
 export BACKENDS_CONSUL_ID="backend:$HOSTNAME"
-PM2=./node_modules/.bin/pm2
 INTERNAL_PORT=$REDIRECTOR_PORT $PM2 start --exp-backoff-restart-delay=100 -i 4 redirector-server.js
 exec -a multienv $PM2-runtime master.js
 # INTERNAL_PORT=$REDIRECTOR_PORT node redirector-server.js &
@@ -49,4 +47,48 @@ exec -a multienv $PM2-runtime master.js
 # node master.js  &
 # multienv_pid=$!
 # wait $multienv_pid
+}
+
+case "$1" in 
+  sh|bash)
+    set -- "$@"
+  ;;
+  multienv)
+    setup_main
+    main
+  ;;
+  resolver)
+    export INTERNAL_PORT=$REDIRECTOR_PORT 
+    exec -a resolver $RUNTIME --exp-backoff-restart-delay=100 -i max redirector-server.js
+  ;;
+  runner)
+    export CLUSTER_CONSUL_ID="cluster:$HOSTNAME"
+    export BACKENDS_CONSUL_ID="backend:$HOSTNAME"
+    exec -a multienv $RUNTIME master.js
+  ;;
+  inspector)
+    exec -a inspector $RUNTIME --exp-backoff-restart-delay=100 -i max k8s-inspector.js
+  ;;
+  dispatcher)
+    exec -a dispatcher $RUNTIME --exp-backoff-restart-delay=100 k8s-dispatcher.js
+  ;;
+  demuxer)
+    exec -a demuxer $RUNTIME --exp-backoff-restart-delay=100 -i max tenant-availability-keeper.js
+  ;;
+  help|--help)
+    echo $0 [command]
+    cat <<-EOF
+      $1 - this messsage
+      bash - run bash
+      multienv - classic setup with master/server, redirector-server.js
+      resolver - just redirector-server.js
+      runner - just master.js
+      inspector - k8s-inspector.js - REST API using ConfigMaps
+      dispatcher - k8s-dispatcher.js
+      demuxer - tenant-availability-keeper.js
+EOF
+  ;;
+esac
+
+
 
