3d5768812d2c9377d65ba7bbd874b71ecb993cc6
Ben West <bewest@gmail.com>
2020-09-16 16:55:17 -0700

    nginx and container helpers for kubernetes

--- start_container.sh
+++ start_container.sh
@@ -33,6 +33,16 @@ npm install
 npm cache verify
 }
 
+function print_multienv_nginx_template ( ) {
+export INTERNAL_PORT=3434
+export REDIRECTOR_PORT=3636
+export RESOLVER_IP=$(dig +short consul.service.consul | ( read ip; test -z "$ip" && echo 8.8.8.8 || echo $ip:8600))
+
+export PORT=4545
+
+erb nginx.conf.erb 
+}
+
 
 PM2=./node_modules/.bin/pm2
 RUNTIME=$PM2-runtime
@@ -49,7 +59,7 @@ exec -a multienv $PM2-runtime master.js
 # wait $multienv_pid
 }
 
-case "$1" in 
+case "${1-help}" in 
   sh|bash)
     set -- "$@"
   ;;
@@ -75,6 +85,46 @@ case "$1" in
   demuxer)
     exec -a demuxer $RUNTIME --exp-backoff-restart-delay=100 -i max tenant-availability-keeper.js
   ;;
+  env)
+    env
+  ;;
+  nginx-for)
+    # allows naming a file to copy the output into but none by default
+    OUT=${3-/dev/null}
+    test -d $(dirname $OUT) && : || mkdir -p $(dirname $OUT)
+    case "$2" in
+      std-multienv)
+        print_multienv_nginx_template | tee $OUT
+      ;;
+      inspector)
+        erb nginx.conf.erb | tee $OUT
+      ;;
+      demuxer)
+        export NGINX_PORT=$PORT
+        export UPSTREAM_SUBREQUEST=${DEMUXER_SERVICE_URI-http://demuxers:3000}
+        # eg 169.254.1.1
+        # eg kube-dns.kube-system.svc.cluster.local
+        export RESOLVER_IP
+        erb nginx.demuxer.conf.erb | tee $OUT
+      ;;
+      resolver)
+        export NGINX_PORT=$PORT
+        export UPSTREAM_SUBREQUEST=${RESOLVER_SERVICE_URI-http://resolvers:3000}
+        export RESOLVER_IP
+        erb nginx.resolver.conf.erb | tee $OUT
+      ;;
+    esac
+  ;;
+  setup_workdir)
+    (
+      cd $WORKER_DIR
+      rm -Rf node_modules/
+      if [[ ! -f .npmrc ]] ; then
+        echo "unsafe-perm = true" | tee .npmrc
+      fi
+      npm install
+    )
+  ;;
   help|--help)
     echo $0 [command]
     cat <<-EOF
@@ -86,6 +136,18 @@ case "$1" in
       inspector - k8s-inspector.js - REST API using ConfigMaps
       dispatcher - k8s-dispatcher.js
       demuxer - tenant-availability-keeper.js
+
+
+      Helpers
+      setup_workdir - (cd \$WORKER_DIR && npm install)
+      nginx-for <container> [/dev/stdout]
+        Print and optionally save nginx configuration for container.
+        * std-multienv - configure legacy nginx config for hybrid runner+resolver
+        * inspector - nginx config for inspector interface
+        * demuxer - nginx config for cluster-wide cluster demuxer
+        * resolver - nginx config for resolver interface
+        
+
 EOF
   ;;
 esac
