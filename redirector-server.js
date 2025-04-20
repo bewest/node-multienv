26218f4aca3a5930f781b07758023def0c09909a
Ben West <bewest@gmail.com>
2025-04-20 12:32:56 -0700

    allow using unix domain socket
    This cleans up a unix domain socket at the end of the process if one is created
    at the beginning.

--- redirector-server.js
+++ redirector-server.js
@@ -3,6 +3,7 @@ var restify = require('restify');
 var url = require('url');
 var bunyan = require('bunyan');
 var dns = require('dns');
+var fs = require('fs');
 
 function createServer (opts) {
   var cluster = opts.cluster;
@@ -141,6 +142,12 @@ function createServer (opts) {
 
 exports = module.exports = createServer;
 
+function remove_socket (socketPath) {
+  if (fs.existsSync(socketPath)) {
+    fs.unlinkSync(socketPath);
+  }
+}
+
 if (!module.parent) {
 var CONSUL_ENV = {
     service: 'backends',
@@ -154,5 +161,13 @@ var CONSUL_ENV = {
   function onConnect ( ) { }
   server.listen(port);
   server.on('listening', console.log.bind(console, 'port', port));
+  process.on('SIGINT', () => {
+    remove_socket(port);
+    process.exit(0);
+  });
+  process.on('SIGTERM', () => {
+    remove_socket(port);
+    process.exit(0);
+  });
 
 }
